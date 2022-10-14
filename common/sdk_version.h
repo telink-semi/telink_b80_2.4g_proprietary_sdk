50ce037a69097a43856f9b1bdd30b0bdd4daed2e
zhen.li <zhen.li@telink-semi.com>
2022-10-14 16:44:37 +0800

    [update]release V3.1.0

--- common/sdk_version.h
+++ common/sdk_version.h
@@ -28,7 +28,7 @@
  * the replace rules is: "$$$B85m_driver_sdk_"#sdk_version_num"$$$", The "#sdk_version_num"
  * will replace with this macro value.
  */
-#define B80_SDK_VERSION_NUM				V3.0.2
+#define B80_SDK_VERSION_NUM				V3.1.0
 
 #define SDK_VERSION_NUM					B80_SDK_VERSION_NUM
 
